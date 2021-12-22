38df9e61a197d0b759444723154949f55eb53f87
colin <colin_schmidt@gmx.de>
2021-12-22 22:54:41 +0100

    adjusts interpunction

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -14,10 +14,10 @@ let player = {
 }
 // Define the Product class - write the Constructor function for Product class here
 function Product(id, name, price, expiryDate) {
-    this.id = id,
-    this.name  = name,
-    this.price  = price,
-    this.expiryDate  = expiryDate
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.expiryDate = expiryDate;
 }
 // Complete the dateDiff function
 const dateDiff = (date1, date2) => {};
