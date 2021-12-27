e59978fd5f9e304ba633c56e302b190033991c3c
colin <colin_schmidt@gmx.de>
2021-12-27 23:36:47 +0100

    Populate product details

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -303,7 +303,7 @@ function init(data) {
         let totalBill = tBill;
         const prId = generateProductId();
         let product = !Object.is(lastProd, undefined) ? lastProd : getProduct(prodList, prId); // Assign the value of product here
-        let productDetails = null; // Assign the value of productDetails here
+        let productDetails = product.getDetails(); // Assign the value of productDetails here
 
         rl.question(`You can buy - ${productDetails}.\n Do you want to buy this item <Y/N>? `.yellow, function (option) {
             const regexYes = null; // Use the RegExp built-in object type here as appropriate
