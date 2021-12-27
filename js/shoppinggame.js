e97121e6d275b6186237134b6a81af0d27b631be
colin <colin_schmidt@gmx.de>
2021-12-27 22:31:04 +0100

    Establish inheritance between Product and MagicProduct

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -45,7 +45,7 @@ function MagicProduct(id, name, price, expiryDate, points, isBonus) {
     this.isBonus = isBonus;
 }
 // Establish inheritance between Product() & MagicProduct() here
-
+MagicProduct.prototype = Object.create(Product())
 // Define Rating class here
 
 // Complete the loadProducts function
