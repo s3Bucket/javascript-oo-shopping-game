cf8374783f5cfb6c7ba1552c588b4b0fa89f54d8
colin <colin_schmidt@gmx.de>
2021-12-27 23:07:19 +0100

    Generate and return a random integer from generateProductId() function

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -211,7 +211,9 @@ const findProductById = (id) => {
 };
 
 // Complete this function
-const generateProductId = () => {};
+const generateProductId = () => {
+    return Math.floor(Math.random() * 20);
+};
 
 
 const getProduct = (prodList, pId) => {
