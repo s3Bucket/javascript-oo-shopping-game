fb427c819edc7b7d63ac57a603133350556cd153
colin <colin_schmidt@gmx.de>
2021-12-27 23:19:48 +0100

    Set the Score of the Player

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -222,7 +222,9 @@ const getProduct = (prodList, pId) => {
 
 
 // Complete this function
-const calculateBill = (prod, tBill) => {};
+const calculateBill = (prod, tBill) => {
+    return prod.price + tBill;
+};
 
 const findPointsToBill = (roundedTotal) => {
     if (roundedTotal > 10 && roundedTotal <= 100) {
@@ -244,12 +246,23 @@ const findPointsToBill = (roundedTotal) => {
 
 
 // Complete this function
-const findPointsForExpDate = (prod) => {};
+const findPointsForExpDate = (prod) => {
+    return (prod.daysToExpire < 30) ? 10 : 0;
+};
 
 
 const calculatePoints = (prod, tBill) => {
     let pointsToBill = findPointsToBill(Math.round(tBill));
     let pointsForExpDate = findPointsForExpDate(prod);
+
+    player.score = player.score + pointsToBill + pointsForExpDate;
+    if(prod instanceof MagicProduct) {
+        if(prod.isBonus) {
+            player.addPoints(prod.points);
+        } else {
+           player.deductPoints(prod.points);
+        }
+    }
 };
 
 // Complete this function
