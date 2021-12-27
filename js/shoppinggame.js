c804bcf94b73222966917a72e2b3d88b4c2b8de1
colin <colin_schmidt@gmx.de>
2021-12-27 22:54:26 +0100

    Find the property names of Product() using the Object.keys() method

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -69,7 +69,7 @@ const loadProducts = (map, prodId) => {
     let a = new Array();
     try {
         // Call Object.keys() to load the property names of the Product object in to prodKeys array here
-        let prodKeys = [];
+        let prodKeys = Object.keys(new Product);
 
         let iterator_obj = map.entries();
 
