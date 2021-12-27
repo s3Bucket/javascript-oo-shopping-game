771f0236ff8f398c3ae07da3cbe867b526afae6f
colin <colin_schmidt@gmx.de>
2021-12-27 22:29:03 +0100

    define MagicProduct object type

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -39,7 +39,11 @@ Product.prototype.getDetails = function() {
     return `Product Name: ${this.name} , Product Price: ${this.price}`
 }
 // Define the MagicProduct class here
-
+function MagicProduct(id, name, price, expiryDate, points, isBonus) {
+    Product.call(this, id, name, price, expiryDate);
+    this.points = points;
+    this.isBonus = isBonus;
+}
 // Establish inheritance between Product() & MagicProduct() here
 
 // Define Rating class here
